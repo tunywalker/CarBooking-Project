9b4546e71b632b0e091764ebd4a5f17b88ff9de3
Tuna GÜL <63519095+tunywalker@users.noreply.github.com>
2024-01-22 04:31:49 +0300

    # Admin Theme implemented

---
+++ Frontends/CarBooking.WebUI/Controllers/AdminCarController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBooking.WebUI.Controllers
+{
+    public class AdminCarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ Frontends/CarBooking.WebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBooking.WebUI.Controllers
+{
+	public class AdminLayoutController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+        public PartialViewResult AdminHeaderPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult AdminNavbarPartial()
+        {
+            return PartialView();
+        }
+    }
+}
