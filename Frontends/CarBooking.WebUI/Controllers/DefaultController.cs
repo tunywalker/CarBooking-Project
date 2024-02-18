b3310e48bc60aa0aa663213e505e01bcaad1947b
Tuna GÜL <63519095+tunywalker@users.noreply.github.com>
2024-02-18 03:40:13 +0300

    # Car Booking filtering operations.

--- Frontends/CarBooking.WebUI/Controllers/DefaultController.cs
+++ Frontends/CarBooking.WebUI/Controllers/DefaultController.cs
@@ -1,12 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBooking.Dto.LocationDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 
 namespace CarBooking.WebUI.Controllers
 {
-    public class DefaultController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class DefaultController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public DefaultController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7182/api/Locations");
+
+			var jsonData = await responseMessage.Content.ReadAsStringAsync();
+			var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);
+			List<SelectListItem> values2 = (from x in values
+											select new SelectListItem
+											{
+												Text = x.Name,
+												Value = x.LocationId.ToString()
+											}).ToList();
+			ViewBag.v = values2;
+
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Index(string book_pick_date, string book_off_date,string time_pick,string time_off,string locationId)
+		{
+			TempData["book_pick_date"] = book_pick_date;
+			TempData["book_off_date"] = book_off_date;
+			TempData["time_pick"] = time_pick;
+			TempData["time_off"] = time_off;
+			TempData["locationId"] = locationId;
+			return RedirectToAction("Index","RentACarList");
+		}
+	}
 }
--- Frontends/CarBooking.WebUI/Controllers/RentACarListController.cs
+++ Frontends/CarBooking.WebUI/Controllers/RentACarListController.cs
@@ -6,9 +6,21 @@ namespace CarBooking.WebUI.Controllers
     {
         public IActionResult Index()
         {
-            var data = TempData["value"];
-            ViewBag.v = data;
-            return View();
+
+            var book_pick_date = TempData["book_pick_date"];
+			var book_off_date = TempData["book_off_date"];
+			var time_pick = TempData["time_pick"];
+			var time_off = TempData["time_off"];
+			var locationId = TempData["locationId"];
+
+
+            ViewBag.bookpickdate = book_pick_date;
+			ViewBag.bookoffdate = book_off_date;
+			ViewBag.timepick = time_pick;
+			ViewBag.timeoff = time_off;
+			ViewBag.locationId = locationId;
+
+			return View();
         }
     }
 }
