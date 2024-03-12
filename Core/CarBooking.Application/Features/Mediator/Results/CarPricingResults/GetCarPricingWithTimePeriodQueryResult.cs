051256be0c7c0e1045326ff5e0d64032e847c293
Tuna GÜL <63519095+tunywalker@users.noreply.github.com>
2024-03-13 02:18:56 +0300

    # CarPricings Page implemented to the project

---
+++ Core/CarBooking.Application/Features/Mediator/Results/CarPricingResults/GetCarPricingWithTimePeriodQueryResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBooking.Application.Features.Mediator.Results.CarPricingResults
+{
+	public class GetCarPricingWithTimePeriodQueryResult
+	{
+        public string Model { get; set; }
+		public decimal DailyAmount { get; set; }
+		public decimal WeeklyAmount { get; set; }
+		public decimal MonthlyAmount { get; set; }
+
+	}
+}
